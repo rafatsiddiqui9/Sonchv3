cfdef6bf7f8eaed6b05823c1edc9946c931167a5
Rafat Siddiqui <mdrafat.siddiqui@outlook.com>
2024-02-14 13:58:01 +0530

    Initial Commit

---
+++ Sonchv3/Sonchv3App.swift
@@ -0,0 +1,17 @@
+//
+//  Sonchv3App.swift
+//  Sonchv3
+//
+//  Created by Rafat on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Sonchv3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sonchv3UITests/Sonchv3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Sonchv3UITestsLaunchTests.swift
+//  Sonchv3UITests
+//
+//  Created by Rafat on 14/02/24.
+//
+
+import XCTest
+
+final class Sonchv3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
